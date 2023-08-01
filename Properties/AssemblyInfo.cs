8e9d7e107146b3353aa136c1f8bf5f43f9d48845
Phaedrus3 <131749747+Phaedrus3@users.noreply.github.com>
2023-08-01 12:00:23 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using MelonLoader;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Better Blackrock: A Fort Blackrock Player Home")]
+[assembly: AssemblyCopyright("Phaedrus")]
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+[assembly: MelonInfo(typeof(BetterBlackrock.Patches), "Better Blackrock: A Fort Blackrock Player Home", "1.0", "Phaedrus, FarcryBliss")]
+
+[assembly: MelonGame("Hinterland", "TheLongDark")]
