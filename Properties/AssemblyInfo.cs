c643763f97c1e0a896249af66e8a6e2b4e401679
Phaedrus3 <131749747+Phaedrus3@users.noreply.github.com>
2023-09-02 12:07:13 -0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Fort Blackrock: A Blackrock Prison Cleanup")]
+[assembly: AssemblyCopyright("Phaedrus")]
+
+[assembly: Guid("f38004bd-0441-4b0f-809c-9af8a9ae16e7")]
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+[assembly: MelonInfo(typeof(BetterBlackrock.Patches), "Fort Blackrock: A Blackrock Prison Cleanup", "1.0", "Phaedrus, FarcryBliss")]
+
+[assembly: MelonGame("Hinterland", "TheLongDark")]
